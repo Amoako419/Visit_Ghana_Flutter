7c5b6cda87991a28c89cc42b38d2bf807bad07ae
amoako419 <ghheskey@gmail.com>
2024-04-29 09:00:27 +0000

    drawer added

--- visit_ghana/lib/screens/home_screen.dart
+++ visit_ghana/lib/screens/home_screen.dart
@@ -15,7 +15,40 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Visit Ghana")),
+      appBar: AppBar(title: const Text("Visit Ghana"),
+      actions: [IconButton(onPressed: (){}, icon: const Icon(Icons.manage_search_outlined))],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const  <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Messages'),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
