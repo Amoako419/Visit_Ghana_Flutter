839eba04619ffdb8efb517a2078b15becc31e4e2
amoako419 <ghheskey@gmail.com>
2024-05-26 23:02:35 +0000

    splash screen edited

--- visit_ghana/lib/screens/splash_screen.dart
+++ visit_ghana/lib/screens/splash_screen.dart
@@ -12,34 +12,73 @@ class Splash extends StatefulWidget{
 
 class _SplashState extends State<Splash> {
   @override
-  void initState(){
-    super.initState();
-    _navigatetohome();
-  }
+  // void initState(){
+  //   super.initState();
+  //   _navigatetohome();
+  // }
 
 
-  _navigatetohome()async{
-    await Future.delayed(const Duration(milliseconds: 8000), () {} );
-    // ignore: use_build_context_synchronously
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>const FirstPage()),);
-  }
+  // _navigatetohome()async{
+  //   await Future.delayed(const Duration(milliseconds: 8000), () {} );
+  //   // ignore: use_build_context_synchronously
+  //   Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>const FirstPage()),);
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          width: 300,
+      body: Container(
+          // width: 500,
+          // height: 400,
+          child: Column(
+            children: [_image(),
+            text(),
+            _login()
+            ],
+          )
+        ),
+      )
+    ;
+  }
+  Widget _image(){
+    return Container(
+         width: 500,
           height: 300,
-         decoration: const BoxDecoration(
+          decoration: const BoxDecoration(
     image: DecorationImage(
     image: AssetImage("assets/images/flag.jpg"),
-    fit: BoxFit.fitWidth,
+    fit: BoxFit.cover
     ),
 ),
-          child: const Text("VISIT GHANA",style: TextStyle(fontSize: 24.0,fontWeight: FontWeight.bold,),),
-        ),
-      ),
+    );
+  }
+
+  Widget _login(){
+    return Container(
+    
+      child:Column(
+        children:[
+       ElevatedButton(onPressed: (){}, child: const Text("Log in",style: TextStyle(color: Colors.white),),
+       style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.blue)),
+       ),
+      ElevatedButton(
+    onPressed: () {
+    // Handle button press
+    },
+    child: const Text("Sign up",style: TextStyle(color: Colors.white)),
+    style: ButtonStyle(
+    backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),// Use MaterialStateProperty for dynamic styling
+    
+    ),
+)
+        ]
+   
+     ) );
+  }
+  Widget text(){
+    return Container(
+      child: Text("VISIT\nGHANA",style: TextStyle(fontSize: 30.0,fontWeight: FontWeight.bold),),
+    padding: EdgeInsets.all(30.0),
     );
   }
 }
